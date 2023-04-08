287c251391940949ccf2dfc1bcebb8286dc3456e
Ezekiel8807 <ayeoluwaseyi@gmail.com>
2023-04-08 23:29:34 +0100

    edit index.js file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import file from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 import cors from 'cors';
 import Express from 'express';
 import * as dotenv from 'dotenv';
@@ -11,6 +13,11 @@ import index from './control/index.js';
 
 //configure environment variable
 dotenv.config();
+
+
+//setting file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 //initiate express
@@ -32,11 +39,10 @@ getpalmoil.get("/api", index);
 
 
 //not found routes
-getpalmoil.use(Express.static('./fontend/build'));
+getpalmoil.use(Express.static(path.join(__dirname + '/fontend/build')));
 getpalmoil.get("*", (req, res) => {
-    res.sendFile("./fontend/build",  "index.html");
+    res.sendFile(path.join(__dirname + '/fontend/build',  'index.html'));
 });
-
 
 //server listening port
 getpalmoil.listen( process.env.PORT, () => console.log("server runing on port " + process.env.PORT));
