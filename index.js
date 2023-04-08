eed203ea3abc0ef1543344c983d0bd76c99952b0
Ezekiel8807 <ayeoluwaseyi@gmail.com>
2023-04-09 00:27:13 +0100

    added new line

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-
 import cors from 'cors';
+import path from 'path';
 import Express from 'express';
 import * as dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
 import bodyParser from 'body-parser';
-
 
 //controllers
 import index from './control/index.js';
 
-
 //configure environment variable
 dotenv.config();
-
 
 //setting file path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 //initiate express
 const getpalmoil = Express();
-
 
 // ========== middle wares =======================
 getpalmoil.use(cors());
@@ -33,10 +27,8 @@ getpalmoil.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 getpalmoil.use(bodyParser.urlencoded({ extended: false }));
 
-
 // express entry point
 getpalmoil.get("/api", index);
-
 
 //not found routes
 getpalmoil.use(Express.static(path.join(__dirname + '/fontend/build')));
