821f750a06d02b3b9966d9aea63bf16a766f3a9c
Ezekiel88o7 <ayeoluwaseyi@gmail.com>
2023-12-18 09:40:47 +0100

    update profile and order page

--- fontend/src/views/profile/Profile.jsx
+++ fontend/src/views/profile/Profile.jsx
@@ -20,38 +20,106 @@ const Profile = ({user}) => {
                     <div class="pro-card-label">
                         <h1 id="Username">{user["user"].username}</h1>
                     </div>
+
+                    <div className="pro-more-action-desktop">
+                        
+                        <div className="pro-change-pass-block">
+            
+                            <h2>Change password</h2>
+
+                            <form action="">
+                                <input type="text" placeholder="Enter New Password"/>
+
+                                <div className="pro-change-passBtn">
+                                    <button type="subnit"> Change</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div className="pro-delete-account">
+                            <button type="button"> Delete Account </button>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="pro-other">
-                    <form className="pro-info">
-                        <div className="pro-info-person">
-                            <h2>Personal Details</h2>
+                    <div className="pro-other-block">
+                        <form className="pro-info">
+                            <div className="pro-info-person">
+                                
+                                <h2 className="pro-heading">Personal Details</h2>
 
-                            <div className="input-box">
-                                <label htmlFor="firstname"></label>
-                                <input type="text" name="firstname" id="firstname" />
+                                <div className="input-box">
+                                    <label htmlFor="firstname">Firstname: </label>
+                                    <input type="text" name="firstname" id="firstname" />
+                                </div>
+
+                                <div className="input-box">
+                                    <label htmlFor="lastname">Lastnane: </label>
+                                    <input type="text" name="lastname" id="lastname" />
+                                </div>
+
+                                <div className="input-box">
+                                    <label htmlFor="phone">Phone number: </label>
+                                    <input type="tel" name="phone" id="phone" />
+                                </div>
+
+                                <div className="input-box">
+                                    <label htmlFor="email">Email address: </label>
+                                    <input type="email" name="email" id="email" />
+                                </div>
+
+                            </div>
+                            <div className="pro-info-shipping">
+                                <h2>Shipping Details</h2>
+
+                                <div className="input-box">
+                                    <label htmlFor="city">City: </label>
+                                    <input type="text" name="city" id="city" />
+                                </div>
+
+                                <div className="input-box">
+                                    <label htmlFor="state">State: </label>
+                                    <input type="text" name="state" id="state" />
+                                </div>
+
+                                <div className="input-box">
+                                    <label htmlFor="country">Country: </label>
+                                    <input type="text" name="country" id="country" />
+                                </div>
+                                
+                                <div className="input-box">
+                                    <label htmlFor="address">Address: </label>
+                                    <input type="text" name="address" id="address" />
+                                </div>
                             </div>
 
-                            <div className="input-box">
-                                <label htmlFor="lastname"></label>
-                                <input type="text" name="lastname" id="lastname" />
+                            <div className="pro-update-button-block">
+                                <button type="submit"> Update </button>
                             </div>
+                            
+                        </form>
+                    </div>
 
-                             <div className="input-box">
-                                <label htmlFor="lastname"></label>
-                                <input type="tel" name="lastname" id="lastname" />
-                            </div>
-
-                        </div>
-                        <div className="pro-info-shipping">
-                            <h2>Shipping Details</h2>
-
-                            <input type="text" name="firstname" placeholder="First Name"/>
-                            <input type="text" name="midname" placeholder="Middle Name"/>
-                            <input type="text" name="lastname" placeholder="Last Name"/>
-                        </div>
+                    <div className="pro-more-action-mobile">
                         
-                    </form>
+                        <div className="pro-change-pass-block">
+            
+                            <h2>Change password</h2>
+
+                            <form action="">
+                                <input type="text" placeholder="Enter New Password"/>
+
+                                <div className="pro-change-passBtn">
+                                    <button type="subnit"> Change</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div className="pro-delete-account">
+                            <button type="button"> Delete Account </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- fontend/src/views/userOrder/UserOrder.jsx
+++ fontend/src/views/userOrder/UserOrder.jsx
@@ -10,14 +10,88 @@ const UserOrder = () => {
     return(
         <div className="userOrder">
             
-            <div className="new-Order">
+            <div className="make-Order">
                 <Order />
             </div>
 
-            <div className="my_order">
-                <div className="recent-order">
-                    <h1>Recent Orders</h1>
+            <div className="new-order">
+
+                <h1>Recent orders</h1>
+
+                <div className="recent-order-container">
+                    <div className="recent_order">
+                        <div className="recent-order-header">
+                            <h3>palmoil</h3>
+                            <i>icon</i>
+                        </div>
+
+                        <div className="recent-order-body">
+                            <div className="recent-order-body-content">
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Quantity: </b> 
+                                    <span> 2 </span> 
+                                </div>
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Quality: </b> 
+                                    <span>5<i>litres</i></span>
+                                </div>
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Amount: </b> 
+                                    <span><i>#</i>50000</span>
+                                </div>
+                            </div>
+
+                            <div className="recent-order-badge">
+                                <span className="state">Processing</span>
+                            </div>
+
+                            <div className="recent-order-actionBtn">
+                                <button type="button">Cancel order</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="recent_order">
+                        <div className="recent-order-header">
+                            <h3>groundnut oil</h3>
+                            <i>icon</i>
+                        </div>
+
+                        <div className="recent-order-body">
+                            <div className="recent-order-body-content">
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Quantity: </b> 
+                                    <span> 2 </span> 
+                                </div>
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Quality: </b> 
+                                    <span>5<i>litres</i></span>
+                                </div>
+
+                                <div className="recent-order-body-content-item">
+                                    <b>Amount: </b> 
+                                    <span><i>#</i>50000</span>
+                                </div>
+                            </div>
+
+                            <div className="recent-order-badge">
+                                <span className="state">Processing</span>
+                            </div>
+
+                            <div className="recent-order-actionBtn">
+                                <button type="button">Cancel order</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
         </div>
     )
